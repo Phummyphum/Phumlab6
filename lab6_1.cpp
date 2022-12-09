9e0d5a645d0687657f002ea5732b561a303b8dc9
Phummyphum <phumrapee.phum@gmail.com>
2022-12-09 15:46:43 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
+    int number=1;
+    int Odd=0;
+    int Even=0;
+    while(number != 0){
     cout << "Enter an integer: ";
+    cin >> number;
+    if(number%2 == 0){
+        Even++ ;
+    }else{
+        Odd++ ;
+    }
+    }
+
     cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cout <<  Even-1;
+    cout << "\n#Odd numbers = ";
+    cout <<  Odd;
     return 0;
 }
